a9e43028ae9ed4283863bc498be192c9b1f48e13
Benny Ho <benny.ho2530@gmail.com>
2020-04-10 16:13:45 +0200

    added operation function

--- app.js
+++ app.js
@@ -9,28 +9,48 @@ function add(value1, value2) {
     return value1 + value2;
 }
 
-console.log(add(value1, value2));
 
-function subtract() {
+function subtract(value1, value2) {
 
 
     return value1 - value2;
 }
 
-console.log(subtract(value1, value2));
 
-function multiply() {
+function multiply(value1, value2) {
 
 
     return value1 * value2;
 }
 
-console.log(multiply(value1, value2));
 
-function divide() {
+
+function divide(value1, value2) {
 
 
     return value1 / value2;
 }
 
-console.log(divide(value1, value2));
+
+
+function operate(op, value1, value2) {
+
+
+    switch(op) {
+
+        case '+':
+            return add(value1, value2);
+            break;
+        case '-':
+            return subtract(value1, value2);
+            break;
+        case '*':
+            return multiply(value1, value2);
+            break;
+        case '/':
+            return divide(value1, value2);
+            break;
+    }
+}
+
+console.log(operate('*', value1, value2));
