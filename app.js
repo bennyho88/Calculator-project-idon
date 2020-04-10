f4fd2b7ca8da9cae3f7a798827caa42ec4ca97e2
Benny Ho <benny.ho2530@gmail.com>
2020-04-10 16:08:49 +0200

    added add, subtract,  multiply and divide function

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 
-* {
-    margin: 0;
-    padding: 0;
+const screen = document.querySelector('#screen');
+
+const value1 = 15;
+const value2 = 2;
+
+function add(value1, value2) {
+
+    return value1 + value2;
 }
+
+console.log(add(value1, value2));
+
+function subtract() {
+
+
+    return value1 - value2;
+}
+
+console.log(subtract(value1, value2));
+
+function multiply() {
+
+
+    return value1 * value2;
+}
+
+console.log(multiply(value1, value2));
+
+function divide() {
+
+
+    return value1 / value2;
+}
+
+console.log(divide(value1, value2));
